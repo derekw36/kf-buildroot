1b8be401ec7d9e5d69fb588a4cbccb2f24f0efdf
Derek Watson <derekw1996@gmail.com>
2023-06-17 12:49:51 -0400

    kmscube-gles2: update to latest
    Upstream fixed builds without GLES3 headers.

--- buildroot-external/package/kmscube-gles2/kmscube-gles2.mk
+++ buildroot-external/package/kmscube-gles2/kmscube-gles2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KMSCUBE_GLES2_VERSION = 345111481d654b38a72b5c3629151dc74f7a82bc
+KMSCUBE_GLES2_VERSION = 0be1681d09e77330e02b8d4708e47a04585f4b16
 KMSCUBE_GLES2_SITE = https://gitlab.freedesktop.org/mesa/kmscube/-/archive/$(KMSCUBE_GLES2_VERSION)
 KMSCUBE_GLES2_LICENSE = MIT
 KMSCUBE_GLES2_LICENSE_FILES = COPYING
